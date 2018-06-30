2dd922a4f2231f59fd68ee4dc733513c8cf3fb28
firoball <unsinnspala@firoball.de>
2018-06-30 13:29:57 +0200

    grrrrr

--- Game/config.h
+++ Game/config.h
@@ -1,7 +1,9 @@
 #define penis short
 
-#define DEBUG
+//#define DEBUG
+//#define WM3_DEBUG
 
 // #define DEBUG_FRAMEWORK_FASTSTART
 
-#define DEBUG_LEVEL "testlevel.wmb"
+//#define DEBUG_LEVEL "testlevel.wmb"
+//#define DEBUG_LEVEL "assettest.wmb"
