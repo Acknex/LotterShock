f84d3a109519073f923862c3cfa3dca18d57eb69
Steffen Pohl <steffen.pohl@gmx.de>
2018-06-30 14:27:44 +0200

    HUD refactoring

--- Game/Source/hud.c
+++ Game/Source/hud.c
@@ -8,8 +8,10 @@
 #define HUD_BARS_XPADDING 3
 
 BMAP * hud_health_label = "health_label.png";
-BMAP * hud_healthbar_background = "healthbar_background.png";
+BMAP * hud_bar_background = "hud_bar_background.png";
 BMAP * hud_healthbar = "healthbar.png";
+BMAP * hud_ammobar = "ammobar.png";
+
 PANEL* HUD_HP_label =
 {
 	bmap = hud_health_label;
@@ -18,32 +20,61 @@ PANEL* HUD_HP_label =
 }
 PANEL* HUD_HP_bars =
 {
-	bmap = hud_healthbar_background;
+	bmap = hud_bar_background;
 	flags = TRANSLUCENT;
 	layer = 2;
 }
 
-PANEL *HUD_elements[HUD_ELEMENT_COUNT];
+PANEL* HUD_Ammo_label =
+{
+	bmap = hud_health_label;
+	flags = TRANSLUCENT;
+	layer = 2;
+}
+PANEL* HUD_Ammo_bars =
+{
+	bmap = hud_bar_background;
+	flags = TRANSLUCENT;
+	layer = 2;
+}
 
 
 void hud_init()
 {
-	HUD_elements [0] = HUD_HP_label;
-	HUD_elements [1] = HUD_HP_bars;
 }
+
+void hud_show_bar(PANEL *label, PANEL *bar, var offsetY) 
+{
+	label->pos_x = HUD_BORDER_PADDING;
+	label->pos_y = screen_size.y - label.size_y - HUD_BORDER_PADDING- offsetY;
+	
+	bar->pos_x = HUD_BORDER_PADDING + label->size_x +3;
+	bar->pos_y = screen_size.y - label->size_y - HUD_BORDER_PADDING;
+	
+	pan_setwindow  (bar, 0, 0,0, 0, bmap_height(hud_healthbar), hud_healthbar, 0,0);
+	
+	set(label, SHOW);
+	set(bar, SHOW);
+}
+
+void hud_update_bar(PANEL *bar, BMAP *source, var current_value, var max_value)
+{
+	var bar_size = bar->size_x-HUD_BARS_XPADDING*2;
+	bar_size *= current_value / max_value;
+	
+	var HUD_BARS_YPADDING = (bar->size_y-bmap_height(source))/2;
+	
+	pan_setwindow  (	bar, 1, 
+							HUD_BARS_XPADDING,HUD_BARS_YPADDING, 
+							bar_size ,bmap_height(source), 
+							source, 
+							0,0);
+}
+
 
 void hud_show()
 {
-    HUD_elements [0]->pos_x = HUD_BORDER_PADDING;
-    HUD_elements [0]->pos_y = screen_size.y - HUD_elements[0].size_y - HUD_BORDER_PADDING;
-    
-    HUD_elements [1]->pos_x = HUD_BORDER_PADDING + HUD_elements[0].size_x +3;
-    HUD_elements [1]->pos_y = screen_size.y - HUD_elements[0].size_y - HUD_BORDER_PADDING;
-    
-    pan_setwindow  (HUD_elements [1], 0, 0,0, 0,bmap_height(hud_healthbar), hud_healthbar, 0,0);
-    
-    set(HUD_elements [0], SHOW);
-    set(HUD_elements [1], SHOW);
+	hud_show_bar(HUD_HP_label, HUD_HP_bars, 0);
 }
 
 void hud_update()
@@ -53,15 +84,5 @@ void hud_update()
 	var PLAYER_HEALTH = 30;
 	//
 	
-	var HUD_HEALTH_SIZE = bmap_width(hud_healthbar_background)-HUD_BARS_XPADDING*2;
-	HUD_HEALTH_SIZE *= PLAYER_HEALTH / PLAYER_MAXHEALTH;
-	
-	var HUD_BARS_YPADDING = (bmap_height(hud_healthbar_background)-bmap_height(hud_healthbar))/2;
-	
-	pan_setwindow  (	HUD_elements [1], 1, 
-							HUD_BARS_XPADDING,HUD_BARS_YPADDING, 
-							HUD_HEALTH_SIZE ,bmap_height(hud_healthbar), 
-							hud_healthbar, 
-							0,0);
-    
+    hud_update_bar(HUD_HP_bars, hud_healthbar, PLAYER_HEALTH, PLAYER_MAXHEALTH);
 }
