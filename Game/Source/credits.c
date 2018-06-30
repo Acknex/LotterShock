9c2c44a0ed43e6eddd21ba14765fcb78dc573ef6
firoball <unsinnspala@firoball.de>
2018-06-30 08:33:05 +0200

    SPALTTER!!!11

--- Game/Source/credits.c
+++ Game/Source/credits.c
@@ -13,12 +13,12 @@ void credits_open()
 	CREDITS_vDone = 0;
 	level_load(""); /* load empty level */
 	wait(1);
-	WM3_load("media\\skaph.wm3");	
+	//WM3_load("media\\skaph.wm3");	
 }
 
 void credits_close()
 {
-	WM3_unload();	
+	//WM3_unload();	
 }
 
 void credits_update()
---
+++ Game/Source/splatter.c
@@ -0,0 +1,40 @@
+#include "splatter.h"
+
+
+void SPLATTER_explode(var count, VECTOR* pos, var distance, BMAP* decal)
+{
+	var i;
+	VECTOR targetPos;
+
+	for (i = 0; i < count; i++)
+	{
+		vec_for_angle(&targetPos, vector(random(360), random(360), 0));
+		vec_scale(&targetPos, distance);
+		vec_add(&targetPos, pos);
+		var mode = IGNORE_ME | IGNORE_YOU | IGNORE_PASSABLE | IGNORE_PASSENTS | IGNORE_PUSH | IGNORE_SPRITES | IGNORE_CONTENT | SCAN_TEXTURE;
+		var dist = c_trace(pos, &targetPos, mode);
+		if(HIT_TARGET)
+		{
+	      PARTICLE* p = ent_decal(you, decal, minv(distance - dist, 50), random(360));
+	      p.lifespan = 160;
+		}
+		
+	}
+}
+
+#ifdef DEBUG
+BMAP* Splattermap = "blutlache.tga";
+ENTITY* splatterent;
+action Splattertest()
+{
+	splatterent = me;
+	
+	while(1)
+	{	
+		if (key_space != 0)
+			SPLATTER_explode(my->skill1, &my->x, my->skill2, Splattermap);
+		
+		wait(1);
+	}
+}
+#endif
