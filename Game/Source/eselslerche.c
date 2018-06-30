40fa1395010ba375fa4d0ae4f25489d786766be2
firoball <unsinnspala@firoball.de>
2018-06-30 13:26:11 +0200

    rumbatzeln

--- Game/Source/eselslerche.c
+++ Game/Source/eselslerche.c
@@ -200,7 +200,8 @@ void ESELSLERCHE__run(ENTITY* ptr)
 
 void ESELSLERCHE__explode(ENTITY* ptr)
 {
-	SPLATTER_explode(50, &ptr->x, ptr->EL_EXPLODEDIST, EL_bmapSplatter);
+	set(ptr, PASSABLE);
+	SPLATTER_explode(100, &ptr->x, 1000, EL_bmapSplatter);
 	//TODO: explode animation
 	ptr->EL_STATE = EL_STATE_DIE;
 	ptr->EL_ANIMSTATE = 0;
--- Game/Source/splatter.c
+++ Game/Source/splatter.c
@@ -11,15 +11,24 @@ void SPLATTER_explode(var count, VECTOR* pos, var distance, BMAP* decal)
 		vec_for_angle(&targetPos, vector(random(360), random(360), 0));
 		vec_scale(&targetPos, distance);
 		vec_add(&targetPos, pos);
-		var mode = IGNORE_ME | IGNORE_YOU | IGNORE_PASSABLE | IGNORE_PASSENTS | IGNORE_PUSH | IGNORE_SPRITES | IGNORE_CONTENT | SCAN_TEXTURE;
+		var mode = IGNORE_PASSABLE | IGNORE_PASSENTS | IGNORE_PUSH | IGNORE_SPRITES | IGNORE_CONTENT | SCAN_TEXTURE | USE_POLYGON;
 		var dist = c_trace(pos, &targetPos, mode);
 		if(HIT_TARGET)
 		{
-	      PARTICLE* p = ent_decal(you, decal, minv(distance - dist, 50), random(360));
+	      PARTICLE* p = ent_decal(you, decal, minv(distance - dist, 500), random(360));
 	      p->lifespan = 160;
+	      p->event = SPLATTER__fade_effect;
 		}
 		
 	}
+}
+
+void SPLATTER__fade_effect(PARTICLE* p)
+{
+	//if (p->lifespan < 40)
+	{
+		p->alpha = 50;//maxv(0, p->alpha - 3* time_step);
+	}	
 }
 
 #ifdef DEBUG
