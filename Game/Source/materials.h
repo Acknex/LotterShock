a3e5dadea8a60446d19f0f7075f0c616496673df
Nils Daumann <nils_daumann@slindev.com>
2018-06-30 15:59:18 +0200

    Level now uses it's shader, blood decals use their own special shader.

---
+++ Game/Source/materials.h
@@ -0,0 +1,60 @@
+#include <mtlView.c>
+
+MATERIAL *matLevel =
+{
+	effect = "Shaders/walls_unlit.fx";
+}
+
+MATERIAL *matFlesh =
+{
+	effect = "Shaders/flesh_basic.fx";
+}
+
+MATERIAL *matDecalBlood =
+{
+	effect = "Shaders/decal_blood.fx";
+}
+
+MATERIAL *matDecalBasic =
+{
+	effect = "Shaders/decal_basic.fx";
+}
+
+MATERIAL *matPPBlurVertical =
+{
+	effect = "Shaders/pp_blur.fx";
+}
+
+MATERIAL *matPPBlurHorizontal =
+{
+	effect = "Shaders/pp_blur.fx";
+}
+
+MATERIAL *matPPCombine =
+{
+	effect = "Shaders/pp_combine.fx";
+}
+
+void SetupPostprocessing()
+{
+	matPPBlurHorizontal.skill1 = floatv(1.0);
+	matPPBlurHorizontal.skill2 = floatv(0.0);
+	
+	matPPBlurVertical.skill1 = floatv(0.0);
+	matPPBlurVertical.skill2 = floatv(1.0);
+	
+	pp_add(matPPBlurVertical);
+	pp_add(matPPBlurHorizontal);
+	pp_add(matPPCombine);
+}
+
+void SetupDefaultMaterials()
+{
+	fog_color = 2;
+	camera.fog_start = 0.0;
+	camera.fog_end = 20000.0;
+	
+	effect_load(mtl_model, "Shaders/walls_unlit.fx");
+	effect_load(mtl_flat, "Shaders/walls_unlit.fx");
+	effect_load(mtl_shaded, "Shaders/walls_unlit.fx");
+}
