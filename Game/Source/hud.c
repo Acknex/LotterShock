b7555f29fd60960d849512b7f4fde32c94b3d39d
Steffen Pohl <steffen.pohl@gmx.de>
2018-06-30 15:14:45 +0200

    Waffenicons

--- Game/Source/hud.c
+++ Game/Source/hud.c
@@ -16,7 +16,6 @@ BMAP * hud_healthbar_bmap = "healthbar.png";
 BMAP * hud_ammo_label_bmap = "ammo_label.png";
 BMAP * hud_ammobar_bmap = "ammobar.png";
 
-BMAP * hud_weapon_icon[4];
 
 PANEL* HUD_crosshair =
 {
@@ -52,26 +51,32 @@ PANEL* HUD_Ammo_bars =
 	layer = 2;
 }
 
+PANEL *hud_weapon_icon[4];
 
 void hud_init()
 {
-	hud_weapon_icon[WEAPON_SWORD] = bmap_create("label_sword.png");
-	hud_weapon_icon[WEAPON_SHOTGUN] = bmap_create("label_shotgun.png");
-	hud_weapon_icon[WEAPON_CELLGUN] = bmap_create("label_cellgun.png");
-	hud_weapon_icon[WEAPON_FLAMETHROWER] = bmap_create("label_flamethrower.png");
+	hud_weapon_icon[WEAPON_SWORD-1] = pan_create("bmap = label_sword.png;",2);
+	hud_weapon_icon[WEAPON_SHOTGUN-1] = pan_create("bmap = label_shotgun.png",2);
+	hud_weapon_icon[WEAPON_CELLGUN-1] = pan_create("bmap = label_cellgun.png",2);
+	hud_weapon_icon[WEAPON_FLAMETHROWER-1] = pan_create("bmap = label_flamethrower.png",2);
 }
 
-void hud_show_bar(PANEL *label, PANEL *bar, var offsetY) 
+void hud_place_label(PANEL *label, var offsetY)
 {
 	label->pos_x = HUD_BORDER_PADDING;
-	label->pos_y = screen_size.y - label.size_y - HUD_BORDER_PADDING- offsetY;
+	label->pos_y = screen_size.y - label.size_y - HUD_BORDER_PADDING - offsetY;
+	
+	set(label, SHOW);
+}
+
+void hud_place_bar(PANEL* label, PANEL *bar, var offsetY) 
+{
 	
 	bar->pos_x = HUD_BORDER_PADDING + label->size_x +3;
 	bar->pos_y = screen_size.y - label->size_y - HUD_BORDER_PADDING -offsetY;
 	
 	pan_setwindow  (bar, 0, 0,0, 0, bmap_height(hud_healthbar_bmap), hud_healthbar_bmap, 0,0);
 	
-	set(label, SHOW);
 	set(bar, SHOW);
 }
 
@@ -93,8 +98,15 @@ void hud_update_bar(PANEL *bar, BMAP *source, var current_value, var max_value)
 void hud_show()
 {
 	var distance_between_bars = HUD_HP_label->size_y + HUD_DIST_BETWEEN_BARS;
-	hud_show_bar(HUD_HP_label, HUD_HP_bars, 0);
-	hud_show_bar(HUD_Ammo_label, HUD_Ammo_bars, distance_between_bars);
+	hud_place_label(HUD_HP_label, 0);
+	
+	int i;
+	for(i = 0; i<4; ++i)
+		hud_place_label(hud_weapon_icon[i], distance_between_bars);
+	
+	
+	hud_place_bar(HUD_HP_label, HUD_HP_bars, 0);
+	hud_place_bar(hud_weapon_icon[0], HUD_Ammo_bars, distance_between_bars);
 	
 	HUD_crosshair->pos_x = (screen_size.x - HUD_crosshair.size_x) /2;
 	HUD_crosshair->pos_y = (screen_size.y - HUD_crosshair.size_y) /2;
@@ -102,16 +114,13 @@ void hud_show()
 	set(HUD_crosshair, SHOW);
 }
 
-BMAP *hud_current_weapon_icon(int id)
+void hud_show_weapon_icon(int id)
 {
-	int id = weapons_get_current();
-	switch(id)
-	{
-		case WEAPON_SWORD:
-		case WEAPON_SHOTGUN:
-		case WEAPON_CELLGUN:
-		case WEAPON_FLAMETHROWER:
-	}
+	int i;
+	for(i = 0; i<4; ++i)
+		reset(hud_weapon_icon[i], SHOW);
+	if(id >= 0)
+		set(hud_weapon_icon[id], SHOW);
 }
 
 void hud_update()
@@ -120,10 +129,18 @@ void hud_update()
 	var PLAYER_MAXHEALTH = 50;
 	var PLAYER_HEALTH = 30;
 	//
-	var ammo = 20;//weapons_get_ammo();
-	var max_ammo = 100;//weapons_get_max_ammo();
+   hud_update_bar(HUD_HP_bars, hud_healthbar_bmap, PLAYER_HEALTH, PLAYER_MAXHEALTH);
+    
+    
+    
+	int weaponID = 1;//weapons_get_current();
 	
-	
-    hud_update_bar(HUD_HP_bars, hud_healthbar_bmap, PLAYER_HEALTH, PLAYER_MAXHEALTH);
-    hud_update_bar(HUD_Ammo_bars, hud_ammobar_bmap, ammo, max_ammo);
+	hud_show_weapon_icon(weaponID-1);
+	if(weaponID > 0)
+	{
+		var ammo = 20;//weapons_get_ammo();
+		var max_ammo = 100;//weapons_get_max_ammo();
+		
+		hud_update_bar(HUD_Ammo_bars, hud_ammobar_bmap, ammo, max_ammo);
+	}
 }
