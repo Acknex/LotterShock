9af2234c746c8718c4c3f7199187f066420f7abc
Steffen Pohl <steffen.pohl@gmx.de>
2018-07-01 02:11:11 +0200

    nochmal Hauptmenü

--- Game/Source/menue_level.c
+++ Game/Source/menue_level.c
@@ -1,11 +1,22 @@
 
 VECTOR menuelevel_center;
 
+
+PANEL* HUD_mainmenue =
+{
+	bmap = "Menuetitle.png";
+	flags = TRANSLUCENT | SHOW;
+	layer = 2;
+}
+
 action menuelevel_anchor()
 {
 	vec_set(&menuelevel_center, my->x);
 	
 	var camera_distance = vec_dist(my->x, camera->x);
+	
+	HUD_mainmenue->pos_x = (screen_size.x-HUD_mainmenue.size_x) /2;
+	HUD_mainmenue->pos_y = 40;
 	
 	while(1)
 	{
