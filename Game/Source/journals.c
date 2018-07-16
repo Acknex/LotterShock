c06a7a8ca3b7fa1cfb352d3e5aa72857b7ce7992
mk1x86 <dev@mk-scape.de>
2018-07-16 18:15:31 +0200

    more story stuff

--- Game/Source/journals.c
+++ Game/Source/journals.c
@@ -289,7 +289,7 @@ void journals_init()
     // Server room enter first time
 
     journals[37].text = "There's the terminal there at the end. Get me there and I'll take a look.";
-    journals[37].media = "Media/acktana_part3_1.mp3";
+    journals[37].media = "Media/story_part3_1.mp3";
     journals[37].type = JOURNAL_TYPE_SUBTITLE;
     journals[37].name = acktana_name;
 
@@ -298,13 +298,13 @@ void journals_init()
     journals[38].text = "Connecting...\n[modem sounds]\nIt seems that the Patchnotes was working on digitization prior to the incident, and when it arrived they uploaded the crew into the computer to escape the Unity Union boarding party. Power requirements for the system increased exponentially with this many people. The system is running on a low power mode. Until we can get it back up, I can't get you any more information.";
     journals[38].type = JOURNAL_TYPE_SUBTITLE;
     journals[38].name = acktana_name;
-    journals[38].media = "Media/acktana_part3_2.mp3";
+    journals[38].media = "Media/story_part3_2.mp3";
     journals[38].followup = 39;
 
     journals[39].text = "Start the generator and talk to the computer people, got it.";
     journals[39].type = JOURNAL_TYPE_SUBTITLE;
     journals[39].name = lotter_name;
-    journals[39].media = "Media/lotter_part3_3.mp3";
+    journals[39].media = "Media/story_part3_3.mp3";
 
     ///////////////////////////////////////////////////////////////////////
     // PART 4
@@ -315,38 +315,38 @@ void journals_init()
     journals[40].text = "Power core output stable at 1.21 Gigawatts. Servers are resuming operation now. Take me back.";
     journals[40].type = JOURNAL_TYPE_SUBTITLE;
     journals[40].name = acktana_name;
-    journals[40].media = "Media/acktana_part4_1.mp3";
+    journals[40].media = "Media/story_part4_1.mp3";
     journals[40].followup = 41;
 
     journals[41].text = " You got it babe.";
     journals[41].type = JOURNAL_TYPE_SUBTITLE;
     journals[41].name = lotter_name;
-    journals[41].media = "Media/lotter_part4_2.mp3";
+    journals[41].media = "Media/story_part4_2.mp3";
 
     // Server terminal
 
     journals[42].text = "[Modem sounds] .... This is terrible. Apparently immediately after uploading themselves, they got attacked by a computer virus by the Unity union. They deployed countermeasures.... something called the... Guardian System? It defeated the Unity Union Virus, but it also trapped them in nullpointer space.";
     journals[42].type = JOURNAL_TYPE_SUBTITLE;
     journals[42].name = acktana_name;
-    journals[42].media = "Media/acktana_part4_3.mp3";
+    journals[42].media = "Media/story_part4_3.mp3";
     journals[42].followup = 43;
 
     journals[43].text = "Anything we can do?";
     journals[43].type = JOURNAL_TYPE_SUBTITLE;
     journals[43].name = lotter_name;
-    journals[43].media = "Media/lotter_part4_4.mp3";
+    journals[43].media = "Media/story_part4_4.mp3";
     journals[43].followup = 44;
 
     journals[44].text = "Jack in, turn off the Guardian and jack out?";
     journals[44].type = JOURNAL_TYPE_SUBTITLE;
     journals[44].name = acktana_name;
-    journals[44].media = "Media/acktana_part4_5.mp3";
+    journals[44].media = "Media/story_part4_5.mp3";
     journals[44].followup = 45;
 
     journals[45].text = " Alright! Let's jack in, kill that jack-off and jack out!";
     journals[45].type = JOURNAL_TYPE_SUBTITLE;
     journals[45].name = lotter_name;
-    journals[45].media = "Media/lotter_part4_6.mp3";
+    journals[45].media = "Media/story_part4_6.mp3";
 
     // interacting with matrix bed
 
